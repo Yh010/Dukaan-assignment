060717f9986f23334d10e0c6036ef2002801b152
Yh010 <yash4success@gmail.com>
2024-10-16 09:21:08 +0530

    endpoints init

--- src/index.js
+++ src/index.js
@@ -8,12 +8,36 @@ app.get('/', (req, res) => {
 
 //seller endpoints
 
-
+app.post('/seller-signup', (req, res) => {
+    
+})
+app.post('/create-seller-store', (req, res) => {
+    
+})
+app.post('/seller-upload-inventory', (req, res) => {
+    
+})
+app.post('/seller-accept-orders', (req, res) => {
+    
+})
 
 //buyer endpoints
 
+app.get('/buyer-store-details', (req, res) => {
+    
+})
 
+app.get('/buyer-product-details', (req, res) => {
+    
+})
 
+app.post('/buyer-add-to-cart', (req, res) => {
+    
+})
+
+app.post('/buyer-place-order', (req, res) => {
+    
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
